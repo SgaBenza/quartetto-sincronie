14821ff4815c88775125cc18ef4098b1e470e68c
sgabenza <sgabenza86@gmail.com>
2019-06-17 23:31:27 +0200

    🌻prepare radius

--- src/components/charts/SoundPressure.js
+++ src/components/charts/SoundPressure.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { range } from 'lodash'
 
-import { CX, CY, PRESSURE_INNER_RADIUS } from '../../constants/chartConstants'
+import {
+  CX,
+  CY,
+  PRESSURE_INNER_RADIUS,
+  PRESSURE_RADIUS,
+  SECONDS,
+} from '../../constants/chartConstants'
 
 export function SoundPressure() {
-  return <circle cx={CX} cy={CY} r={PRESSURE_INNER_RADIUS} stroke={'green'} fill={'red'} />
+  return (
+    <>
+      <circle cx={CX} cy={CY} r={PRESSURE_INNER_RADIUS} stroke={'green'} fill={'red'} />
+      {range(SECONDS).map((d, i) => (
+        <line
+          key={d + i}
+          x1={CX}
+          y1={CY}
+          x2={CX}
+          y2={CY - PRESSURE_RADIUS}
+          stroke="green"
+          transform={`rotate(${(360 / SECONDS) * (d + 1)} ${CX} ${CY}) translate(0 -70)`}
+        />
+      ))}
+    </>
+  )
 }
--- src/constants/chartConstants.js
+++ src/constants/chartConstants.js
@@ -1,9 +1,13 @@
-export const HEIGHT = 500
-export const WIDTH = 500
+export const HEIGHT = 1000
+export const WIDTH = 1000
 
 export const CX = WIDTH / 2
 export const CY = HEIGHT / 2
 
+export const TRANSLATE_CENTER = `translate(${CX} ${CY})`
+
+export const SECONDS = 184
+
 // SOUND PRESSURE CHART
 export const PRESSURE_INNER_RADIUS = 70
-export const PRESSURE_OUTER_RADIUS = 400
+export const PRESSURE_RADIUS = 100
